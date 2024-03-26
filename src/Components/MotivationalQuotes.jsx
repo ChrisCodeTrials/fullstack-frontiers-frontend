850de060ba22740a2748c3a24d629331a9d80a35
Jaden G <banko@Jadens-MacBook-Air.local>
2024-03-26 15:59:28 -0400

    initial form changes

--- src/Components/MotivationalQuotes.jsx
+++ src/Components/MotivationalQuotes.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import {useState} from 'react'
 
 const MotivationalQuotes = () => {
+  const [quote, setQuote] = useState("")
+  const [createdAuthor, setCreatedAuthor] = useState("")
+  const [inputedCategory, setInputedCategory] = useState("")
+  const [createdQuote, setCreatedQuote] = useState([])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setQuote(...e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+
+
+
+
   return (
-    <div>MotivationalQuotes</div>
+    <div className='form-page'>
+      <h1 className='form-header'>Create Quote</h1>
+      <form onSubmit={handleSubmit} className="form-container">
+            <div>
+              <label htmlFor="user-quote" className="form-user-quote" />
+              <textarea
+                id="user-quote"
+                required
+                value={quote}
+                onChange={handleChange}
+                placeholder="Enter Quote..."
+                rows="4"
+                className=""
+              />
+            </div>
+            <div className="">
+              <input
+                type="text"
+                required
+                id="user-name"
+                placeholder="Author"
+                
+                onChange={handleChange}
+                className=""
+              />
+              <br/>
+              <button
+                type="submit"
+                className="form-submit-btn"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+    </div>
   )
 }
 
